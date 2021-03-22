9a871ab65243461b23c39c00d580a8403633d359
keshavcodex <keshav789123@gmail.com>
2021-03-22 10:36:07 +0530

    README File added

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -44,18 +44,6 @@ class Game{
 public class GuessTheNumber {
 public static void main(String [] args) {
 
-/*
- 38
-  Guess a class Game, Which allows a user to play "Guess the Number"
-   game once. Game should have the following the methods:
-   1. Constructor to generate the random number
-   2. takeuserInput() to take a user input of number
-   3. isCorrectNumber() to detect whether the number entered by the user is ture
-   4. getter and setter for noOfGuesses
-   Use properties such as noOfGuesses(int), etc to get this task done!
- */
-	
-	
 	Game g=new Game();
 	boolean b=false;
 	while(!b) {
---
+++ README.txt
@@ -0,0 +1,7 @@
+  Guess a class Game, Which allows a user to play "Guess the Number"
+   game once. Game should have the following the methods:
+   1. Constructor to generate the random number
+   2. takeuserInput() to take a user input of number
+   3. isCorrectNumber() to detect whether the number entered by the user is ture
+   4. getter and setter for noOfGuesses
+   Use properties such as noOfGuesses(int), etc to get this task done!
